c3dc5a2ec057d19c43e5ce6cdd175ef855cc7671
shadowpr1est <alisher.arginbekov@mail.ru>
2025-06-12 15:15:26 +0500

    docker-compose update

--- backend/database.py
+++ backend/database.py
@@ -7,3 +7,10 @@ DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@db:5432
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
